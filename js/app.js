d81b3e46560adec7e785cd3bde75a1a9bbd5db48
Ramesh Paudel <rpaudel58@gmail.com>
2019-06-25 16:27:44 +0545

    Added string concatenation, alert and prompt

--- js/app.js
+++ js/app.js
@@ -24,14 +24,14 @@ console.warn("This will show warning message")
 
 console.error("This will show error message")
 
-console.log( 1 + 2);
+console.log(1 + 2);
 
-console.log( 18 / 2);
-console.log( 16 - 2 - 2 - 2);
-console.log(9*5);
+console.log(18 / 2);
+console.log(16 - 2 - 2 - 2);
+console.log(9 * 5);
 
 //Remainder
-console.log("Remainder is calculated by % symbol ",11 % 5);
+console.log("Remainder is calculated by % symbol ", 11 % 5);
 
 
 var math = 90;
@@ -41,4 +41,36 @@ var english = 50;
 var totalObtainedMarks = math + science + english;
 var totalMarks = 300;
 
-console.log( totalObtainedMarks/totalMarks * 100)
+console.log(totalObtainedMarks / totalMarks * 100)
+
+// =================================================================
+
+
+
+var firstName = "Ram"
+var middleName = "Bdr"
+var lastName = "Doe";
+
+var fullName = firstName + " " + middleName + " " + lastName
+
+
+//To check the length of characters use .length in string
+
+console.log("The total caracter count is ",fullName.length);
+
+
+//Conversion of string to capital letters
+console.log("Name in all caps ",fullName.toUpperCase());
+
+//Conversion of string to small letters
+console.log("Name in all small ",fullName.toLowerCase());
+
+
+//Alert ==> Halting and showing information
+
+// alert("Hello World")
+
+
+//Propmpt asks user for question 
+// The result will be saved in the variable
+var question = prompt("Enter your name ");
