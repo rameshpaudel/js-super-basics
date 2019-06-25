06f7b445ab63cfaf133c06f626b7220893a8a164
Ramesh Paudel <rpaudel58@gmail.com>
2019-06-25 15:09:31 +0545

    Variable and console message with basic arithmetics

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//3 level of log message
+//1 -> Informative message
+//2 -> Warning message
+//3 -> Error message
+
+//Variable
+var someNumber = 10;
+
+var someText = "Hello World";
+
+console.log(someNumber);
+
+console.log(someText);
+
+
+console.log(10);
+
+console.log(20);
+
+console.log("This is a string that will show up in the console");
+
+
+console.warn("This will show warning message")
+
+console.error("This will show error message")
+
+console.log( 1 + 2);
+
+console.log( 18 / 2);
+console.log( 16 - 2 - 2 - 2);
+console.log(9*5);
+
+//Remainder
+console.log("Remainder is calculated by % symbol ",11 % 5);
+
+
+var math = 90;
+var science = 30;
+var english = 50;
+
+var totalObtainedMarks = math + science + english;
+var totalMarks = 300;
+
+console.log( totalObtainedMarks/totalMarks * 100)
