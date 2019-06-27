83e9759e23d91f7276d788c6e32dcfbd60c2c035
Ramesh Paudel <rpaudel58@gmail.com>
2019-06-27 14:54:43 +0545

    Added Or in the conditionals

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,45 +1,29 @@
-var firstNum = 50;
-var secondNum = 20;
-
-// console.log(firstNum > secondNum);
-/*
-*This is a multiline comment
-*/
-
-//This is a single line comment
-if( firstNum > secondNum ){
-    console.log("The first number is greater")
-}
-
-
-
-
 // Asking user for the value
 var firstString = prompt("Enter a number")
 var secondString = prompt("Enter another number")
 
-console.log("The type of variable before the conversion " , typeof firstString)
+// console.log("The type of variable before the conversion " , typeof firstString)
 
 // Conversion of string to number
 var firstNum = parseInt(firstString)
 var secondNum = parseInt(secondString)
 
-console.log("The type of variable after the conversion ", typeof firstNum)
+// console.log("The type of variable after the conversion ", typeof firstNum)
 
 //isNaN is used to check weather the value is in fact a number
 // isNaN -> true ==> Not a number 
 // isNaN -> false ==> Is a number 
 
-if(isNaN(firstNum)){
-    console.error("The first number that you've entered is not valid")
+
+if(isNaN(firstNum) || isNaN(secondNum)  ){
+    //The user input a NaN value
+    console.error("Both input has to be a number ")
+} else {
+    //We check the numbers here
+    if(firstNum > secondNum){
+        console.log("The first num is greater with the value of ", firstNum)
+    } else{
+        console.log("The second num is greater with the value of ", secondNum)
+    }
+
 }
-
-if(firstNum > secondNum){
-    console.log("The first num is greater with the value of ", firstNum)
-} else{
-    console.log("The second num is greater with the value of ", secondNum)
-}
-
-
-
-// var parsedData = parseInt("Some random text");
