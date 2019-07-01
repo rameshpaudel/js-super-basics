687d19cf1e16ac00f9fceb67cc5432a3f1dbbd34
Ramesh Paudel <rpaudel58@gmail.com>
2019-07-01 16:09:56 +0545

    Adding items to the begining and end of array

--- js/array101.js
+++ js/array101.js
@@ -6,3 +6,20 @@ console.log("Last item ", arr[arr.length -1 ])
 console.log("The full size of the array is ", arr.length)
 
 
+// Looping all items from an array
+
+for(var i = 0; i < arr.length; i++){
+    console.log("Index: ", i," Value: ", arr[i])
+}
+
+
+var newArr = [];
+//Adding items to the end of the array
+newArr.push(1);
+newArr.push("Hello");
+newArr.push("World");
+newArr.push("Test");
+
+
+//Adding items to the begining of the array
+newArr.unshift("Ram")
