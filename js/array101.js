8938e4a5f72b926ebc1066b345b72bfea23602b4
Ramesh Paudel <rpaudel58@gmail.com>
2019-07-04 15:39:32 +0545

    Added remove functionaliry

--- js/array101.js
+++ js/array101.js
@@ -1,3 +1,4 @@
+/* 
 var arr = [1, 2, 3, 4, 5, 6, "Test", "Another", "Super"];
 
 console.log("The first item in the array is ",arr[0])
@@ -5,9 +6,7 @@ console.log("The second item in the array is ",arr[1])
 console.log("Last item ", arr[arr.length -1 ])
 console.log("The full size of the array is ", arr.length)
 
-
 // Looping all items from an array
-
 for(var i = 0; i < arr.length; i++){
     console.log("Index: ", i," Value: ", arr[i])
 }
@@ -34,3 +33,25 @@ newArr.pop();
 
 //Removing the first item from the array
 newArr.shift();
+
+*/
+
+//Removing multiple items
+var numCollection = [];
+
+for (var i = 1; i <= 1000; i++) {
+    numCollection.push(i)
+}
+
+//How many items we want to remove from the array
+var itemsToRemove = 5;
+
+//Total number of items in the array
+var totalNoOfItems = numCollection.length;
+
+//Determining the start position of the array
+var indexPosition = totalNoOfItems - itemsToRemove;
+
+
+//To remove multiple items from the array
+numCollection.splice(indexPosition, itemsToRemove);
