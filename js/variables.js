e5b7610530710a7996471d62a10c1f6e9d48cefb
Ramesh Paudel <rpaudel58@gmail.com>
2019-07-10 15:03:44 +0545

    Basic variables

---
+++ js/variables.js
@@ -0,0 +1,40 @@
+//Variable
+var test = 1235
+var test = "              1235 Hello World                "
+
+let temp = "12345"
+const theConst = "Any value"
+
+
+//Basic string functions
+console.log(test)
+//To count the length of characters
+console.log(test.length)
+
+console.log(test.trim())
+
+
+console.warn("THis outputs a warning")
+console.error("THis outputs an error message")
+
+
+
+var testArr = [
+    "Test","Another","Random",4,5,6
+];
+
+//TO check the data type => typeof 
+
+console.log('type', testArr.length, typeof testArr)
+
+
+for(var i=0;i<testArr.length; i++){
+
+    console.log(testArr[i])
+
+
+
+}
+
+
+
