6255755af627e0ffa89298da67bb277c6ac18652
Ramesh Paudel <rpaudel58@gmail.com>
2019-07-10 16:02:03 +0545

    Added basic question and conditionals

--- js/variables.js
+++ js/variables.js
@@ -32,13 +32,38 @@ console.log('type', testArr.length, typeof testArr)
 // i += 2
 
 
-for (var i = 0; i < 100; i += 2) {
-    console.log(i)
-}
+// for (var i = 0; i < 100; i += 2) {
+//     console.log(i)
+// }
 
 
-var i = 0;
-while (i < 100) {
-    console.log(i)
-    i += 2;
+// var i = 0;
+// while (i < 100) {
+//     console.log(i)
+//     i += 2;
+// }
+
+
+// Ask 2 questions
+// SHow which number was greater
+// parseInt() -> string to number
+var question1 = prompt("Enter a number")
+var question2 = prompt("Enter anotheer number")
+
+
+console.log(typeof question1, typeof question2)
+
+var firstQuestion = parseInt(question1)
+var secondQuestion = parseInt(question2)
+
+// console.log(typeof firstQuestion, typeof secondQuestion)
+
+console.log("The sum of the inputs is" ,firstQuestion + secondQuestion)
+
+if(firstQuestion > secondQuestion){
+    console.log("The first question has greater value which is",firstQuestion)
+} else if(firstQuestion == secondQuestion){
+    console.log("Both of the values are equal")
+} else{
+    console.log("The second question has greater value which is", secondQuestion)
 }
